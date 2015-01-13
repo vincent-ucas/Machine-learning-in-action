5bc863ca20c9d8bcbc490931b88ca4ad382bfe66
vincent he <vincent.ucas@gmail.com>
2015-01-13 15:40:35 +0800

    refine standar regression

--- Regression/regression.py
+++ Regression/regression.py
@@ -14,11 +14,11 @@ def loadDataSet(filename):
     numFeat = len(open(filename).readline().split('\t')) - 1
     dataMat = []; labelMat = []
     fr = open(filename)
-    for line in fr.readline():
+    for line in fr.readlines():
         lineArr = []
         curLine = line.strip().split('\t')
         for i in range(numFeat):
-            lineArr.append(float(curLine(i)))
+            lineArr.append(float(curLine[i]))
         dataMat.append(lineArr)
         labelMat.append(float(curLine[-1]))
     return dataMat, labelMat
