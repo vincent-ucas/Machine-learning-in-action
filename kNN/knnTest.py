eb2e2efb16b0dd674a131d3567e75834d707cf3c
vincent he <vincent.ucas@gmail.com>
2015-01-14 23:00:30 +0800

    add dating Classifier testing

--- kNN/knnTest.py
+++ kNN/knnTest.py
@@ -15,5 +15,22 @@ def dataAnalyzing():
                15.0*array(datingDataLabel), 15.0*array(datingDataLabel))
     plt.show()
 
+# dating classfying test with kNN
 
-if __name__ == '__main__' : dataAnalyzing()
+def datingClassTest():
+    testRatio = 0.10    # extract 10% data for testing, others for training
+    datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVal = kNN.autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTest = int(m * testRatio)
+    errorCount = 0.0
+
+    for i in range(numTest):
+        classifierResult = kNN.kNNClassify(normMat[i, :], normMat[numTest:m, :], datingLabels[numTest:m], 3)
+        print "classify: %d, the real classify is: %d" % (classifierResult, datingLabels[i])
+        if classifierResult != datingLabels[i]: errorCount += 1.0
+
+    print "Total error rate is %f" % (errorCount/float(numTest))
+
+
+if __name__ == '__main__' : datingClassTest()
