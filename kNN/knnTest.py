ddd7fae89ef0b503df7b20d5ff274e898e5ce16e
vincent he <vincent.ucas@gmail.com>
2015-01-14 22:30:33 +0800

    add dating data analyzing with matplotlib.

--- kNN/knnTest.py
+++ kNN/knnTest.py
@@ -1,3 +1,19 @@
-import kNN2
+import kNN2, kNN
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import *
 
-kNN2.testHandWritingClass()
+def kNN_test():
+    kNN2.testHandWritingClass()
+
+def dataAnalyzing():
+    # open a data file 'datingTestSet.txt'
+    datingDataMat, datingDataLabel = kNN.file2matrix('datingTestSet2.txt')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1],
+               15.0*array(datingDataLabel), 15.0*array(datingDataLabel))
+    plt.show()
+
+
+if __name__ == '__main__' : dataAnalyzing()
