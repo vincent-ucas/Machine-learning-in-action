2794d7edc4dffaaa966c461ec119d185cececb9e
vincent he <vincent.ucas@gmail.com>
2015-01-16 21:29:23 +0800

    updated decision tree testing program

--- decisionTree/DtreeTest.py
+++ decisionTree/DtreeTest.py
@@ -1,4 +1,13 @@
+__author__ = 'Vincent HE'
+"""
+    $author:      Ming
+    $date:        2015/01/16
+    $Description: Testing for decision tree classifier.
+"""
+
 import Dtree
 
 dataSet, labels= Dtree.createDataSet()
-Dtree.calShannonEnt(dataSet)
+#Dtree.calShannonEnt(dataSet)
+print Dtree.selectBestFeat(dataSet)
+print dataSet
