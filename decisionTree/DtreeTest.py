4913aa33eed960afd441c510ee8e74946db839ad
vincent he <vincent.ucas@gmail.com>
2015-01-17 22:24:30 +0800

    updated Dtree testing, add another example of forecasting invisible glasses style

--- decisionTree/DtreeTest.py
+++ decisionTree/DtreeTest.py
@@ -7,15 +7,32 @@
 import Dtree
 import treePlotter
 
-# load data to create decision tree
-dataSet, labels= Dtree.createDataSet()
-decision_tree = Dtree.createDtree(dataSet, labels)
+def test1():
+    # load data to create decision tree
+    dataSet, labels= Dtree.createDataSet()
+    decision_tree = Dtree.createDtree(dataSet, labels)
 
-# plot tree
-treePlotter.plotDecisionTree(decision_tree)
+    # plot tree
+    treePlotter.plotDecisionTree(decision_tree)
 
-# classify
-dataSet, labels= Dtree.createDataSet()
-print Dtree.classify(decision_tree, labels, [1, 0])
-print Dtree.classify(decision_tree, labels, [1, 1])
+    # classify
+    dataSet, labels= Dtree.createDataSet()
+    print Dtree.classify(decision_tree, labels, [1, 0])
+    print Dtree.classify(decision_tree, labels, [1, 1])
 
+    # store
+    Dtree.storeTree(decision_tree, 'classifierStorage_DTree.txt')
+    Dtree.grabTree('classifierStorage_DTree.txt')
+
+
+def test2():
+    fr = open('lenses.txt')
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lenses_labels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lenses_tree = Dtree.createDtree(lenses, lenses_labels)
+
+    print lenses_tree
+    treePlotter.plotDecisionTree(lenses_tree)
+
+
+if __name__ == '__main__' : test2()
